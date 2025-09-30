e35812204d7a1972adc1509a0fb995fae3aab2b3
Viduranga G.J.S <131736074+Janith-sh@users.noreply.github.com>
2025-09-30 17:40:47 +0530

    Revert "landing,login,signin frontend"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AutoCareLandingPage from './pages/customer/Landing';
-import Login from './pages/auth/Login';
-import Signup from './pages/auth/Signup';
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AutoCareLandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
+    <>
+      <div>
+        <h1>AutoServe</h1>
+        <p>Automobile Service Management System</p>
+      </div>
+    </>
   )
 }
 
